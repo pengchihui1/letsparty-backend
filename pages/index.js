858da091fb0e1354251fa9e077472903243fadd2
XTC-02102162112\Administrator <pengchihui1@gmail.com>
2021-06-15 23:31:59 +0800

    111

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,6 @@ const Home = () => {
   // const q = encodeURIComponent(query)// 编码UTF-8
   const [data, setData] = useState('')
   useEffect(() => {
-    // let timeoutURL = ''
-    // if (data) {
-    //   clearTimeout(timeoutURL)
-    //   timeoutURL = setTimeout(() => {
-    //     http.get('/api/get_word', { params: { q: data } })
-    //       .then((res) => {
-    //         console.log(res)
-    //       })
-    //       .catch((error) => {
-    //         console.log(error)
-    //       })
-    //   }, 500)
-    // }
-    // if (!data) {
     http.get('/api/get_word', { params: { q: data } })
       .then((res) => {
         console.log(res.data.trans_result)
@@ -26,7 +12,6 @@ const Home = () => {
       .catch((error) => {
         console.log(error)
       })
-    // }
   }, [data])
   return (
     <textarea onChange={(e) => { setData(e.target.value) }} />
